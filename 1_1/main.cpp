f6faa04aa987dc8f078806605019e60ba317f528
Duc Than <unityloper@gmail.com>
2022-03-15 22:08:02 +0000

    Bit manipulation with all lower case characters

--- 1_1/main.cpp
+++ 1_1/main.cpp
@@ -2,18 +2,21 @@
 #include <string>
 #include <unordered_map>
 
-bool isUnique(std::string str) {
-    if (str.size() > 128) return false;
-    std::unordered_map<char, int> charCounter;
-    for (int i=0; i<str.size(); i++){
-        if (charCounter.find(str[i]) != charCounter.end()) return false;
-        charCounter[str[i]] = 1;
+bool isUnique(std::string str)
+{
+    int checkingBits = 0;
+    for (int i = 0; i < str.size(); i++)
+    {
+        if (checkingBits & (1 << (str[i] - 'a')))
+            return false;
+        checkingBits |= 1 << (str[i] - 'a');
     }
     return true;
 }
 
-int main() {
+int main()
+{
     std::cout << "Is Unique" << std::endl;
-    std::string result = isUnique("abca") ? std::string("") : std::string("not ");
+    std::string result = isUnique("abc") ? std::string("") : std::string("not ");
     std::cout << "abc is " << result << "unique";
 }
